b18dc8a9120f305678a7464f1f592c17c78c90dd
guilhermepama <guilhermepama1@gmail.com>
2025-01-07 11:37:26 -0300

    cadastro e delete clientes

--- catalogo/src/main.tsx
+++ catalogo/src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './pages/home'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './pages/home';
+import '/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <Home /> 
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
+
+<BrowserRouter
+
+/>
--- catalogo/src/pages/home/index.tsx
+++ catalogo/src/pages/home/index.tsx
@@ -1,10 +1,118 @@
-function Home (){
-return (
-    <div>
-        <h1>Home</h1>
-    </div>
-)
+import { useEffect, useState, useRef, FormEvent } from "react"
+import { FiTrash } from "react-icons/fi"
+import { api } from "../../services/api"
 
+interface CustomerProps {
+    id: number;
+    nome: string;
+    email: string;
+    status: boolean;
+    created_at: string;
 }
 
-export default Home;
+export default function Home (){
+
+    const [customers, setCustomers] = useState<CustomerProps[]>([])
+    const nameRef = useRef<HTMLInputElement | null>(null)
+    const emailRef = useRef<HTMLInputElement | null>(null)
+
+    useEffect(() => {
+        loadCustomers();
+    }, [])
+
+
+    async function loadCustomers(){
+        const response = await api.get('/customers')
+        setCustomers(response.data);
+    }
+
+    async function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        if(!nameRef.current?.value || !emailRef.current?.value){
+            alert("Preencha todos os campos");
+            return;
+        }
+
+        const response = await api.post('/customer', {
+            nome: nameRef.current?.value,
+            email: nameRef.current?.value,
+        });
+
+        setCustomers(allCustomers => [...allCustomers, response.data]);
+
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+    }
+
+   async function handleDelete(id: number){
+        try {
+            await api.delete("/customer", {
+                params: {
+                    id: id,
+                }
+            })
+
+            const allCustomers = customers.filter ((customer) => customer.id !== id )
+            setCustomers(allCustomers) 
+ 
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    return (
+    <div className="w-full min-h-screen bg-violet-950 flex justify-center px-4">
+        <main className="my-10 w-full bg-violet-800 md:max-w-2xl p-4 rounded-3xl" >
+            <h1 className="text-4xl font-medium text-white">Clientes</h1>
+
+            <form className="flex flex-col my-6" onSubmit={handleSubmit}>
+                <label className="font-medium text-white">Nome:</label>
+                <input
+                type="text"
+                placeholder="Digite seu nome..."
+                className="w-full mb-5 p-2 rounded-lg"
+                ref={nameRef}
+                />
+
+                <label className="font-medium text-white">Email:</label>
+                <input
+                type="temail"
+                placeholder="Digite seu email..."
+                className="w-full mb-5 p-2 rounded-lg"
+                ref={emailRef}
+                />
+                
+                <input
+                type="submit"
+                value="cadastrar"
+                className="cursor-pointer w-full p-2 bg-green-400 rounded-lg" />
+                               
+            </form> 
+
+            <section className="flex flex-col gap-4">
+                {customers.map ( (customer) => (
+                    <article key={customer.id} className="w-full bg-violet-400 p-4 rounded-lg relative hover:scale-105 transition duration-300">
+                    <p><span className="font-medium">Nome: </span>{customer.nome}</p>
+                    <p><span className="font-medium">Email: </span>{customer.email}</p>
+                    <p><span className="font-medium">Status: </span>{customer.status ? "Ativo" : "Inativo"}</p>
+  
+                    <button
+                        className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-xl absolute right-0 -top-2"
+                        onClick={ () => handleDelete(customer.id)}
+                    >
+                        <FiTrash size={18} color="#fff" />
+                    </button>
+                
+                </article>
+                ))}    
+            </section>      
+
+
+        </main>
+       
+    </div>
+    )
+
+}
